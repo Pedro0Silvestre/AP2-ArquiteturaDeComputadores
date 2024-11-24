60a134b8d257727f21df8298b37a42d98ca89de8
Pedro Henrique <161378527+Pedro0Silvestre@users.noreply.github.com>
2024-11-23 23:20:03 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -9,16 +9,101 @@
 .DEF input = r20
 .DEF somaCaractere = r21
 .DEF comparador = r22
+.DEF contaAparicao = r23
+
+; TAREFA 1 CRIANDO TABELA DOS DIGITOS
+
+;carregand os valores nos registradores
+inicioTabelaCaracteres:
+ldi  caractere,0x30;inicando com o valor 0x30 correspondente ao digito 0
+ldi termo,0x1
+ldi flag,0xA
+ldi r27,0x02 ; apontadores do endereço incialmente 0x200
+ldi r26,0x00
+ldi somaCaractere,0xFF; R21 como FF para configurar os pinos como saída
+out DDRD, somaCaractere; Configura todos os pinos de PORTD como saída
+clr somaCaractere;limpa o registrador
+
+tabelaDigito:
+st X,caractere
+out PORTD,caractere;imprimi caractere na saida
+add caractere,termo
+inc contador 
+inc r26
+cp flag,contador
+brne tabelaDigito
+
+;REATRIBUINDO OS VALORES INICIAIS
+ldi caractere,0X41 ;codigo A maiusculo
+ldi flag,0X1A
+ldi contador,0x0
+
+tabelaMaiusculo:
+;Criando tabela maiuscula
+st X,caractere
+out PORTD,caractere;imprimi caractere na saida
+add caractere,termo
+inc contador 
+inc r26
+cp flag,contador
+brne tabelaMaiusculo
+
+;REATRIBUINDO OS VALORES INICIAIS
+ldi caractere,0X61 ;codigo a minusuculo
+ldi contador,0x0 ;redefinindo contador
+
+tabelaMinusculo:
+;Criando tabela minuscula
+st X,caractere
+out PORTD,caractere;imprimi caractere na saida
+add caractere,termo
+inc contador 
+inc r26
+cp flag,contador
+brne tabelaMinusculo
+
+;ARMAZENANDO O ESPAÇO EM BRANCO E O ESC
+ldi caractere,0X20
+st X,caractere
+out PORTD,caractere;imprimi caractere na saida
+inc r26
+ldi caractere,0x1B
+st X,caractere
+out PORTD,caractere;imprimi caractere na saida
+
+
+;TAREFA 0: DEFINIR O LOOP PRINCIPAL QUE AGUARDA O CÓDIGO DE ENTRADA PARA INICIAR UMA DAS ROTINAS 
+inicio:
+clr r25
+out DDRC,r25; define PORTC como entrada
+in r25,PINC ; Lê o valor no pinc e armazena em r25
+cpi r25,0X1C;compara entrada com o código do incio de montagem da tabela
+breq tabelaInputInicio
+cpi r25,0X1D
+breq iniciarContaCaracteres
+cpi r25,0X1E
+breq iniciarAparicaoCaractere
+rjmp inicio
+
+iniciarContaCaracteres:
+rjmp inicioContaCaracteres
+iniciarAparicaoCaractere:
+rjmp aparicaoCaractereInicio
+
 
 ;TAREFA 2
 ;DEFININDO AS PORTAS
+tabelaInputInicio:
 clr input         ; R20 como 0 para configurar os pinos como entrada
 out DDRC, r20    ; Configura todos os pinos de PORTC como entrada
-ldi r27,0x03
-ldi r26,0x00
+ldi somaCaractere,0xFF; R21 como FF para configurar os pinos como saída
+out DDRD, somaCaractere; Configura todos os pinos de PORTD como saída
+clr somaCaractere;limpa o registrador
+ldi r29,0x03
+
 
 ;LENDO TABELA INPUT
-tabelaInputInicio:
+inicioLeitura:
 ;DEFININDO AS PORTAS
 clr input         ; R20 como 0 para configurar os pinos como entrada
 out DDRC, r20    ; Configura todos os pinos de PORTC como entrada
@@ -31,27 +116,31 @@ verficarFimCadeia:
 cpi input,0X1B ;verifica se é ESC
 breq fimleitura ; Sai da leitura se for esc
 
-verifcarCaractereValido: ; verifica sw o input corresponde a um carctere imprimivel em ascii
+verifcarCaractereValido: ; verifica se o input corresponde a um carctere imprimivel em ascii
 cpi input,0X20 ; compara o valor do input com o começo da tabela 
-brlo tabelaInputInicio ;retorna a entrada até o caractere ser válido
+brlo inicioLeitura ;retorna a entrada até o caractere ser válido
 cpi input,0X7F ;compara a entrada com o final da tabela 
-brsh aparicaoCaractereInicio;tabelaInputInicio ;se for maior ou igual retorna ao inicio
+brsh inicioLeitura;tabelaInputInicio ;se for maior ou igual retorna ao inicio
 rjmp verificaArmazenamento
 
 verificaArmazenamento:;Verifica se o limiite do armazenamento chegou
-cpi r26,0XFF
-brne armazenainput
+cpi r28,0XFF
+breq inicio; caso a tabela esteja cheia atualiza os 10 mais frequentes
 
 armazenainput:
-st X,input
-inc r26
+st Y,input
+out PORTD,input;imprimi o caractere armazenado na porta D
+inc r28
 rjmp leitura
 
 fimleitura:
 ldi input,0X20 ; tribui o espaço em branco
-st X,input ; armazena o espaço em branco 
-inc r26
-rjmp tabelaInputInicio
+st Y,input ; armazena o espaço em branco 
+inc r28
+rjmp inicio;termina a leitura de caracteres atualizando a tabela de frequencia
+
+
+
 
 ;TAREFA3:determinar o número de caracteres presentes na tabela de sequência de caracteres. 
 
@@ -69,7 +158,7 @@ contaCaracteres:
 ld caractere,X; 1 - armazenar valor do caracter na tabela num registrador
 cpi caractere,0X20;2- cericar se caractere é um espaço em branco
 breq espacoEmBranco; ignora o espaço
-cpi caractere,0X00;Verifica se o espaço está vazio caso esteja significa q a tabela já foi preenchida 
+cpi caractere,0X00;Verifica se o espaço está vazio caso esteja significa o fim da tabela
 breq imprimirCaractere ;caso chegue no fim da tabele direciona até a impressão
 inc somaCaractere ;se for um caractere valido acrescenta um no contador de soma
 sts 0X401,somaCaractere;armazena valor da soma no endereço
@@ -81,6 +170,7 @@ rjmp contaCaracteres
 
 imprimirCaractere:;5- imprimir numa porta de saida
 out PORTD,somaCaractere
+rjmp inicio;volta para o inicio
 
 
 ; TAREFA 4: ler um novo byte correspondente a um caractere em uma porta de entrada e contar o número de vezes que o caractere está presente na tabela de sequência de caracteres
@@ -109,7 +199,7 @@ brsh caractereInvalido;tabelaInputInicio ;se for maior ou igual retorna ao inici
 comparacao:
 ld comparador,X ;3.1 - armazenar o valor da tabela caractere num registrador
 cpi comparador,0X00 ;Verifica se chegou ao fim da tabela
-breq imprimiComparacao
+breq armazenaComparacao
 cp comparador,input ;3.2 compara com o valor de input
 breq aparicao;3.3- Se o comparador for igual direciona a aparicao
 inc r26;3-4 Se não for igual continua a verificar até chegar no fim da tabela
@@ -122,10 +212,13 @@ rjmp comparacao
 ;4- ARMAZENAR RESULTADO NA MEMÓRIA(0X402)
 armazenaComparacao:
 sts 0X402,contador
+rjmp imprimiComparacao
 ;5 - LIBERA NA PORTA DE SAIDA
 imprimiComparacao:
 out PORTD,contador
 clr r26; reinicia o percorrer da tabela
+clr contador;reinicia o contador de aparições
+rjmp inicio;volta ao inicio após imprimir o numero de aparicoes
 
 ;(SE NÃO) LER NOVAMENTE(PASSO 1)
 caractereInvalido:
@@ -133,58 +226,7 @@ rjmp aparicaoCaractere
 
 
  
-; TAREFA 1 CRIANDO TABELA DOS DIGITOS
 
-;carregand os valores nos registradores
-inicio:
-ldi  caractere,0x30;inicando com o valor 0x30 correspondente ao digito 0
-ldi termo,0x1
-ldi flag,0xA
-ldi r27,0x02 ; apontadores do endereço incialmente 0x200
-ldi r26,0x00
 
-tabelaDigito:
-st X,caractere
-add caractere,termo
-inc contador 
-inc r26
-cp flag,contador
-brne tabelaDigito
-
-;REATRIBUINDO OS VALORES INICIAIS
-ldi caractere,0X41 ;codigo A maiusculo
-ldi flag,0X1A
-ldi contador,0x0
-
-tabelaMaiusculo:
-;Criando tabela maiuscula
-st X,caractere
-add caractere,termo
-inc contador 
-inc r26
-cp flag,contador
-brne tabelaMaiusculo
-
-;REATRIBUINDO OS VALORES INICIAIS
-ldi caractere,0X61 ;codigo a minusuculo
-ldi contador,0x0 ;redefinindo contador
-
-tabelaMinusculo:
-;Criando tabela minuscula
-st X,caractere
-add caractere,termo
-inc contador 
-inc r26
-cp flag,contador
-brne tabelaMinusculo
-
-;ARMAZENANDO O ESPAÇO EM BRANCO E O ESC
-ldi caractere,0X20
-st X,caractere
-inc r26
-ldi caractere,0x1B
-st X,caractere
-
-break
 
  
